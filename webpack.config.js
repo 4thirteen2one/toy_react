f513c8a81965466e66d14171efd3fb3149ece8c2
4thirteen2one <4thirteen2one@gmail.com>
2020-11-01 15:17:18 +0000

    npm install babel-loader @babel/core @babel/preset-env --save-dev; use babel-loader and preset

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,18 @@ module.exports = {
     mode: "development",
     optimization: {
         minimize: false
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
+            }
+        ]
     }
 }
