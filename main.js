9c1de9aa90a7f967e6258aa30a6f1ef537fa37f2
4thirteen2one <4thirteen2one@gmail.com>
2020-11-02 22:35:51 +0800

    add custom component

--- main.js
+++ main.js
@@ -1,23 +1,17 @@
+import { createElement, Component, render } from "./toy_react.js"
+
 for(let i of [1, 2, 3]) {
     console.log(i);
 }
 
-function createElement(tagName, attributes, ...children) {
-    let e = document.createElement(tagName);
-    for (let p in attributes) {
-        e.setAttribute(p, attributes[p]);
+class MyComponent extends Component {
+    render() {
+        return <div>my_component</div>
     }
-    for (let child of children) {
-        if (typeof child === "string") {
-            child = document.createTextNode(child);
-        }
-        e.appendChild(child);
-    }
-    return e;
 }
 
-document.body.appendChild(<div id="b" class="c">
+render(<MyComponent id="b" class="c">
     <div>hello</div>
     <div>world</div>
-</div>);
+</MyComponent>, document.body);
 
